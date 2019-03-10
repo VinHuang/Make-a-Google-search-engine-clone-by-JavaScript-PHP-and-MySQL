bffd0f1666e41a8933f506c11a0afbf6ea02a9d9
ZJ <shadowmirror@gmail.com>
2019-03-10 23:56:16 +0800

    study php code

--- classes/DomDocumentParser.php
+++ classes/DomDocumentParser.php
@@ -1,22 +1,37 @@
 <?php
 class DomDocumentParser {
 
-	private $doc;
+	// private $doc;
 
 	public function __construct($url) {
 
 		$options = array(
 			'http'=>array('method'=>"GET", 'header'=>"User-Agent: doodleBot/0.1\n")
-			);
+            );
+        // 建立一個 array 它 key 是 'http' , 
+        // value 是另外一個新建立 array，內層的 array 有兩個 key 是 'mehtod' 與 'header',
+        // 粗箭頭後面是他們的對應 value
+        // 這些是 stream_context_create 的 options 格式
+
 		$context = stream_context_create($options);
+        // stream_context_create 可以模擬 get / post 行為
 
 		$this->doc = new DomDocument();
-		@$this->doc->loadHTML(file_get_contents($url, false, $context));
+        @$this->doc->loadHTML(file_get_contents($url, false, $context));
+        // file_get_contents 可以把文件讀入一個 string 裡
+        // DomDocument 可以在 php 更好的操作 DOM
+        // $this->doc->loadHTML() 則會暫時存在 $this->doc 上
+        // 這樣可以用 $this->doc 做 DOM 操作
 	}
 
 	public function getlinks() {
-		return $this->doc->getElementsByTagName("a");
-	}
+        return $this->doc->getElementsByTagName("a");
+        // $this->doc 是 new DomDocument(); 的新實例，
+        // 後面 getElementsByTagName() 應該是指使用 
+        // 類別 DomDocument 內建的方法 getElementsByTagName()
+        // 所以這個方法的用途應該就是回傳某一頁所有的 <a>
+    }
+    // 在 crawl.php :9 被使用
 
 }
 ?>
--- crawl.php
+++ crawl.php
@@ -1,19 +1,29 @@
 <?php
 include("classes/DomDocumentParser.php");
+// 引入DomDocumentParser
 
 function followLinks($url) {
 
 	$parser = new DomDocumentParser($url);
+    // 函數每執行一次，都會新建立一個 DomDocumentParser 實體。
+    // $url 會在 DomDocumentParser 的 __construct($url) 當作參數傳入
 
-	$linkList = $parser->getLinks();
+    $linkList = $parser->getLinks();
+    // 執行 類別 DomDocumentParser 的 公開方法 getlinks()
+    // ，並用 $linkList 儲存 getLinks() return 回來的值
+    // 這個方法的用途應該就是回傳某一頁所有的 <a>
+    // 所以會是某一頁所有 link 的集合
 
 	foreach($linkList as $link) {
-		$href = $link->getAttribute("href");
-		echo $href . "<br>";
+        $href = $link->getAttribute("href");
+        // 使用 $link 的類別方法 getAttribute() 取得 <a> 的 href 屬性值
+        echo $href . "<br>";
+        // 印出靠DomDocumentParser類別方法getLinks()收集到的 <a> href 值並換行
 	}
 
 }
 
-$startUrl = "http://www.apple.com";
+$startUrl = "https://pkq.herokuapp.com/poke-profile/124#recipe";
 followLinks($startUrl);
+// 做一個function 放入指定參數
 ?>
